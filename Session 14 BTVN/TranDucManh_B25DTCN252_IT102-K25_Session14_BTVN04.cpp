31b4713058db5368587b1d6e49a297f004095212
ducmanht6-hub <ducmanht6@gmail.com>
2025-11-11 07:17:17 +0700

    Update TranDucManh_B25DTCN252_IT102-K25_Session14_BTVN04.cpp

--- Session 14 BTVN/TranDucManh_B25DTCN252_IT102-K25_Session14_BTVN04.cpp
+++ Session 14 BTVN/TranDucManh_B25DTCN252_IT102-K25_Session14_BTVN04.cpp
@@ -11,8 +11,9 @@ void printArray(int arr[], int n) {
 int main() {
     int arr[] = {1, 2, 3, 4, 5};
     int size;
-    int kichThuoc = sizeof(arr) / sizeof(arr[0]);
+    int size = sizeof(arr) / sizeof(arr[0]);
     printArray(arr, size);
     return 0;
 }
+
 
